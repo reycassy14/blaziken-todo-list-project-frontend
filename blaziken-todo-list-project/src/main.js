cbe8c58bef303eb55ce922033ef69d91dbbd60f5
reycassy14 <reycassy122@gmail.com>
2025-11-25 09:12:33 +0800

    Update main.js

--- blaziken-todo-list-project/src/main.js
+++ blaziken-todo-list-project/src/main.js
@@ -2,18 +2,6 @@ import './style.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-<<<<<<< Updated upstream
-
-import router from './router';
-//import { MotionPlugin } from '@vueuse/motion';
-
-const app = createApp(App);
-
-//app.use(MotionPlugin).use(router)
-app.use(router).mount('#app')
-
-// app.mount('#app')
-=======
 // import VueLazyLoad from 'vue-lazyload'
 import router from './router';
 import { MotionPlugin } from '@vueuse/motion';
@@ -23,4 +11,3 @@ const app = createApp(App);
 app.use(MotionPlugin).use(router)
 
 app.mount('#app')
->>>>>>> Stashed changes
